0134aee4b13bb5736bf7cba09059ac637ddbb48b
CHCHALLE <Chrischalle@yahoo.com>
2024-07-15 23:41:48 +0000

    first time

--- src/app.js
+++ src/app.js
@@ -2,10 +2,64 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = [
+  ["♦", "diamond"],
+  ["♥", "heart"],
+  ["♠", "spade"],
+  ["♣", "club"]
+];
+
+const cardValue = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  const randomSuitImage = suits[suitIndex][0];
+  const randomSuitClass = suits[suitIndex][1];
+  const randomCardValueIndex = Math.floor(Math.random() * cardValue.length);
+  const displayCardValue = cardValue[randomCardValueIndex];
+
+  const topLeftSuitDivElement = document.querySelector(".top-left-suit");
+  const bottomRightSuitDivElement = document.querySelector(
+    ".bottom-right-suit"
+  );
+  const cardValueDivElement = document.querySelector(".cardValue");
+
+  if (topLeftSuitDivElement == null) {
+    console.log("no top left suit element found");
+    return;
+  }
+  if (bottomRightSuitDivElement == null) {
+    console.log("no bottom right suit element found");
+    return;
+  }
+  if (cardValueDivElement == null) {
+    console.log("no cardValue element found");
+    return;
+  }
+  if (randomSuitClass == "diamond" || randomSuitClass == "heart") {
+    cardValueDivElement.classList.add("redNumber");
+  }
+  console.log(randomSuitClass);
+
+  cardValueDivElement.innerText = displayCardValue;
+  bottomRightSuitDivElement.innerText = randomSuitImage;
+  bottomRightSuitDivElement.classList.add(randomSuitClass);
+  topLeftSuitDivElement.innerText = randomSuitImage;
+  topLeftSuitDivElement.classList.add(randomSuitClass);
 };
